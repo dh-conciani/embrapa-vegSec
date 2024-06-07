95459a080866a67bd728c089c6244904bfa87754
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2024-06-07 08:52:29 -0300

    Update 01_getTransitions.js

--- 01_getTransitions.js
+++ 01_getTransitions.js
@@ -13,64 +13,56 @@ var secVeg = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas
 // set reference year
 var year = 2021;
 
-// get secondary vegetation for the reference year
-var secVeg = secVeg.select('secondary_vegetation_age_' + year)
-  .selfMask();
-
-// get class in the reference year
-var destinationClass = collection.select('classification_' + year)
-  .updateMask(secVeg);
-  
-// get year of transition
-var transitionYear = ee.Image(year - 1).subtract(secVeg).selfMask();
-
 // set list of years to be assessed
 var yearsList = ee.List.sequence({'start': 1985, 'end': year}).getInfo();
 
-// create empty recipe
-var sourceClass = ee.Image(0);
-// for each year
-yearsList.forEach(function(year_i) {
-  // get mask
-  var mask = transitionYear.updateMask(transitionYear.eq(year_i));
-  // get class
-  var x = collection.select('classification_' + year_i)
-    .updateMask(mask);
-  // store
-  sourceClass = sourceClass.blend(x).selfMask();
-});
+// for each ecoregion 
+ecoregions.map(function(feature) {
+  // get secondary vegetation for the reference year
+  var secVeg_f = secVeg.select('secondary_vegetation_age_' + year)
+    .clip(feature)
+    .selfMask();
+  
+  // get class in the reference year
+  var destinationClass = collection.select('classification_' + year)
+    .updateMask(secVeg_f);
+    
+  // get year of transition
+  var transitionYear = ee.Image(year - 1).subtract(secVeg_f).selfMask();
+  
+  // create empty recipe
+  var sourceClass = ee.Image(0);
+  // for each year
+  yearsList.forEach(function(year_i) {
+    // get mask
+    var mask = transitionYear.updateMask(transitionYear.eq(year_i));
+    // get class
+    var x = collection.select('classification_' + year_i)
+      .updateMask(mask);
+    // store
+    sourceClass = sourceClass.blend(x).selfMask();
+  });
+  
+  // combine layers into a single image
+  var output = ee.Image(0)
+    .add(destinationClass).multiply(100)
+      .add(secVeg_f).multiply(100)
+        .add(sourceClass);
+  
+  // export
+  return Export.image.toAsset({
+      "image": output.toInt32(),
+      "description": feature.get('ID') + '_v1',
+      "assetId": 'users/dh-conciani/embrapa_trajs/' + feature.get('ID') + '_v1',
+      "scale": 30,
+      "pyramidingPolicy": {
+          '.default': 'mode'
+      },
+      "maxPixels": 1e13,
+      "region": feature.geometry()
+  });  
+})
 
-// combine layers into a single image
-var output = ee.Image(0)
-  .add(destinationClass).multiply(100)
-    .add(secVeg).multiply(100)
-      .add(sourceClass);
-
-// export
-Export.image.toAsset({
-    "image": output.toInt32(),
-    "description": 'secondary_vegetation_trajs_by_age_v1',
-    "assetId": 'users/dh-conciani/embrapa_trajs/secondary_vegetation_trajs_by_age_v1',
-    "scale": 30,
-    "pyramidingPolicy": {
-        '.default': 'mode'
-    },
-    "maxPixels": 1e13,
-    "region": ecoregions.geometry()
-});  
 
 
-// import the color ramp module from mapbiomas 
-var palettes = require('users/mapbiomas/modules:Palettes.js');
-var vis = {
-    'min': 0,
-    'max': 62,
-    'palette': palettes.get('classification7')
-};
 
-Map.addLayer(destinationClass, vis, 'destination class');
-Map.addLayer(secVeg, {palette: ['red', 'yellow', 'green'], min: 1, max:20}, 'secondary age');
-//Map.addLayer(transitionYear.randomVisualizer(), {}, 'transition year')
-//Map.addLayer(collection, {}, 'collection', false);
-Map.addLayer(sourceClass, vis, 'source class');
-Map.addLayer(output.randomVisualizer(), {}, 'output', false)
