adb07120cc4505998185c8dc5c1461976c76c021
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2024-06-07 09:17:22 -0300

    Update 01_getTransitions.js

--- 01_getTransitions.js
+++ 01_getTransitions.js
@@ -3,7 +3,7 @@
 
 // load ecoregions
 var ecoregions = ee.FeatureCollection('users/dh-conciani/help/embrapa/ecoregions');
-
+print('ecoregions',ecoregions);
 // read mapbiomas collection
 var collection = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1');
 
@@ -17,51 +17,58 @@ var year = 2021;
 var yearsList = ee.List.sequence({'start': 1985, 'end': year}).getInfo();
 
 // for each ecoregion 
-ecoregions.map(function(feature) {
-  // get secondary vegetation for the reference year
-  var secVeg_f = secVeg.select('secondary_vegetation_age_' + year)
-    .clip(feature)
-    .selfMask();
-  
-  // get class in the reference year
-  var destinationClass = collection.select('classification_' + year)
-    .updateMask(secVeg_f);
+ecoregions.aggregate_array('ID').evaluate(function(IDs){
+  IDs.forEach(function(ID){
     
-  // get year of transition
-  var transitionYear = ee.Image(year - 1).subtract(secVeg_f).selfMask();
-  
-  // create empty recipe
-  var sourceClass = ee.Image(0);
-  // for each year
-  yearsList.forEach(function(year_i) {
-    // get mask
-    var mask = transitionYear.updateMask(transitionYear.eq(year_i));
-    // get class
-    var x = collection.select('classification_' + year_i)
-      .updateMask(mask);
-    // store
-    sourceClass = sourceClass.blend(x).selfMask();
+    var feature = ecoregions.filter(ee.Filter.eq('ID',ID));
+    
+    // get secondary vegetation for the reference year
+    var secVeg_f = secVeg.select('secondary_vegetation_age_' + year)
+      .clip(feature)
+      .selfMask();
+    
+    // get class in the reference year
+    var destinationClass = collection.select('classification_' + year)
+      .updateMask(secVeg_f);
+      
+    // get year of transition
+    var transitionYear = ee.Image(year - 1).subtract(secVeg_f).selfMask();
+    
+    // create empty recipe
+    var sourceClass = ee.Image(0);
+    // for each year
+    yearsList.forEach(function(year_i) {
+      // get mask
+      var mask = transitionYear.updateMask(transitionYear.eq(year_i));
+      // get class
+      var x = collection.select('classification_' + year_i)
+        .updateMask(mask);
+      // store
+      sourceClass = sourceClass.blend(x).selfMask();
+    });
+    
+    // combine layers into a single image
+    var output = ee.Image(0)
+      .add(destinationClass).multiply(100)
+        .add(secVeg_f).multiply(100)
+          .add(sourceClass);
+    
+    // export
+    var region = feature.geometry();
+    
+    Export.image.toAsset({
+        "image": output.toInt32(),
+        "description": ID + '_v1',
+        "assetId": 'users/dh-conciani/embrapa_trajs/' + feature.get('ID').getInfo() + '_v1',
+        "scale": 30,
+        "pyramidingPolicy": {
+            '.default': 'mode'
+        },
+        "maxPixels": 1e13,
+        "region": region
+    });  
   });
-  
-  // combine layers into a single image
-  var output = ee.Image(0)
-    .add(destinationClass).multiply(100)
-      .add(secVeg_f).multiply(100)
-        .add(sourceClass);
-  
-  // export
-  return Export.image.toAsset({
-      "image": output.toInt32(),
-      "description": feature.get('ID') + '_v1',
-      "assetId": 'users/dh-conciani/embrapa_trajs/' + feature.get('ID') + '_v1',
-      "scale": 30,
-      "pyramidingPolicy": {
-          '.default': 'mode'
-      },
-      "maxPixels": 1e13,
-      "region": feature.geometry()
-  });  
-})
+});
 
 
 
