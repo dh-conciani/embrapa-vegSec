eb06c50b6a2fb6246159ef8a6b5b2f02a60adff8
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2024-07-22 15:28:39 -0300

    Update 01_vegSec_ecoRegion.js

--- 01_vegSec_ecoRegion.js
+++ 01_vegSec_ecoRegion.js
@@ -5,10 +5,7 @@
 var sec_veg = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_deforestation_secondary_vegetation_v2'); 
 
 // define years
-var years = [1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 
-             1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
-             2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
-             2021, 2022];
+var years = [1986, 1991, 1996, 2001, 2006, 2011, 2016, 2021];
 
 // build recipe
 var recipe = ee.Image([]);
